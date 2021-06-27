2d1f0b72f4090dd9b628d1d6b9874e9bed1c7b74
Mateusz Pyzowski <managervcf@gmail.com>
2021-06-27 12:10:26 +0800

    Rename enums.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
-export * from './enums/disks.enum';
-export * from './enums/modes.enum';
-export * from './enums/players.enum';
+export * from './enums/disk.enum';
+export * from './enums/mode.enum';
+export * from './enums/player-type.enum';
 export * from './enums/play-again.enum';
 
 export * from './interfaces/answers.interface';
